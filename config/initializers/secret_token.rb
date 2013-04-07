8ed436aeea003090b83c08222d8c4b0c67c7568b
Chris Polis <cmpolis@gmail.com>
2013-04-07 14:27:28 -0700

    initial commit - empty Rails project with ActiveMerchant

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCommerce::Application.config.secret_token = '2a2b5a95a9716deca2a8ff6044730620af2d3a4d88cf7f1901cd4dc6a34d21f7285ca744ffe39a4588da7b8bbd1a376031f757ffb0f77a773826c78e708e826e'
